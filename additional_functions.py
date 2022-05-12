14b995d2953db0ce98f8346cb62bf03c8690184d
bhassani <bhassani@users.noreply.github.com>
2022-05-11 19:01:42 -0500

    added XOR function for our payloads

--- additional_functions.py
+++ additional_functions.py
@@ -1,5 +1,24 @@
 #more functions that we might need
 
+def XOR_ENCRYPT3():
+    #sample payload
+    payload = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+    xor_key = "\x58\x58\x11\x62"
+
+    length = len(payload)
+
+    cipher_payload = ""
+    for i in range(length):
+        t = payload[i]
+        k = xor_key[i % len(xor_key)]
+        x = ord(k) ^ ord(t)
+        ordinal = "%02x" % x
+        cipher_payload += ''.join(ordinal)
+    print(cipher_payload)
+
+        
+
+
 #https://github.com/bjornedstrom/elliptic-curve-chemistry-set/blob/master/eddsa.py
 def le2int(buf):
     """little endian buffer to integer."""
